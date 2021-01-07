bae6f1500b4177dc0d9b74210abd7a3850ba15e1
Kamil Nahotko <kamilnahotko@gmail.com>
2021-01-07 18:21:28 +0100

    Update form to register endpoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8080/api/auth";
 
 ReactDOM.render(<App />, document.getElementById("root"));
