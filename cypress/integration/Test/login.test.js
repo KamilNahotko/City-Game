db86980f9cb992287f2d23498a264df3f5d4735b
sebastian_cwirko <cwirko.sebastian@gmail.com>
2021-01-11 13:14:39 +0100

    login test

---
+++ cypress/integration/Test/login.test.js
@@ -0,0 +1,43 @@
+const faker = require('faker');
+
+const urlMain = 'http://localhost:3000/'
+const urlHome = 'http://localhost:3000/home'
+
+const pass = [
+    {email: faker.internet.email(), password:faker.internet.password()},
+    {email: faker.internet.email(), password:faker.internet.password()},
+    {email: faker.internet.email(), password:faker.internet.password()},
+    {email: faker.internet.email(), password:faker.internet.password()},
+]
+
+
+describe('Main Page', () => {
+    context('uruchomienie storny i sprawdzanie czy wszystko jest ok',()=>{
+        it('City Game visit',()=>{
+            cy.visit(urlMain)
+        })
+    })
+    context("Sprawdzenie czy sa inputy",()=>{
+        it('Sprawdzanie czy sa inputy',()=>{
+            cy.get(':nth-child(4) > .MuiButtonBase-root').click()
+        })
+    })
+    context("logowanie",()=>{
+        
+        pass.map((item)=>{
+            it('Wpisywanie',()=>{
+                clearInput()
+                cy.get('#email').type(item.email)
+                cy.get('#password').type(item.password)
+                cy.get('.Login-form-10 > .MuiButtonBase-root').click()
+                cy.url().should('eq', urlHome)
+                cy.visit(urlMain)
+            })
+        })
+    })
+})
+const clearInput = () =>
+{
+    cy.get('#email').type('{selectall}{del}')
+    cy.get('#password').type('{selectall}{del}')
+}
