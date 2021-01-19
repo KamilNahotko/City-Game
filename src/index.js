81060deaf6e54ef4f7cfd57173d16034a1d8e9ed
sebastian_cwirko <cwirko.sebastian@gmail.com>
2021-01-19 10:14:05 +0100

    change api url to serwer 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:8080/api/";
-axios.defaults.baseURL = "http://citygame.ga:8080/api/";
+axios.defaults.baseURL = "http://34.122.22.62:8080/api/";
+// axios.defaults.baseURL = "http://citygame.ga:8080/api/";
 
 ReactDOM.render(<App />, document.getElementById("root"));
